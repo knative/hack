138fcb9159e9210c08dd4909b530a3b43c998dc3
Scott Nichols <snichols@vmware.com>
2020-11-30 15:20:27 -0800

    do not edit GOPATH

--- library.sh
+++ library.sh
@@ -578,15 +578,14 @@ function go_update_deps() {
 # Return a GOPATH to a temp directory. Works around the out-of-GOPATH issues
 # for k8s client gen mixed with go mod.
 # indended to be used like:
-#   export GOPATH=$(go_mod_path)
-function go_mod_path() {
+#   export GOPATH=$(go_mod_gopath_hack)
+function go_mod_gopath_hack() {
 	export MODULE_NAME=$(go mod graph | cut -d' ' -f 1 | grep -v '@' | head -1)
-	export TMP_DIR="$(mktemp -d)"
-	export GOPATH=${TMP_DIR}
-	export TMP_REPO_PATH="${TMP_DIR}/src/${MODULE_NAME}"
+	local TMP_DIR="$(mktemp -d)"
+	local TMP_REPO_PATH="${TMP_DIR}/src/${MODULE_NAME}"
 	mkdir -p "$(dirname "${TMP_REPO_PATH}")" && ln -s "${REPO_ROOT_DIR}" "${TMP_REPO_PATH}"
 
-	echo "$GOPATH"
+	echo "${TMP_DIR}"
 }
 
 # Run kntest tool, error out and ask users to install it if it's not currently installed.
