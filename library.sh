71211b5de63b3b3fb11553a197be4660e57c25d1
Jimmy Lin <zhongduo@google.com>
2021-01-18 09:36:00 -0500

    add git tag regex

--- library.sh
+++ library.sh
@@ -797,7 +797,7 @@ function latest_version() {
   if [ $(current_branch) = "master" ] || [ $(current_branch) = "main" ]; then
     # For main branch, simply use git tag without major version, this will work even
     # if the release tag is not in the main
-    git tag | sort -r --version-sort | head -n1
+    git tag -l "v[0-9]*" | sort -r --version-sort | head -n1
   else
     local semver=$(git describe --match "v[0-9]*" --abbrev=0)
     local major_minor=$(echo "$semver" | cut -d. -f1-2)
